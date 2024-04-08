911b02b8ca62ea81373b9de5a69a27755179571d
SergioPC <sergiolb@hotmail.es>
2024-04-08 15:44:27 -0500

    Cuarto commit

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -9,4 +9,23 @@ router.post("/animals", (req, res) => {
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
+
+router.get("/animals", (req, res) => {
+    animalSchema.find()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.put("/animals/:id", (req, res) => {
+    animalSchema.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.delete("/animals/:id", (req, res) => {
+    animalSchema.findByIdAndDelete(req.params.id)
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
 module.exports = router;
