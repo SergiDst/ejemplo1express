787d765c412687f8b2dcd32b35c4f0e34d187d42
SergioPC <sergiolb@hotmail.es>
2024-04-15 16:18:20 -0500

    Quinto Commit
    Se agrego una autenticación, creación y login de usuario

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -1,3 +1,4 @@
+const verifyToken = require('./validate_token');
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
@@ -6,6 +7,12 @@ router.post("/animals", (req, res) => {
     const animal = animalSchema(req.body);
     animal
         .save()
+        .then((data) => res.json(data))
+        .catch((error) => res.json({ message: error }));
+});
+
+router.get("/animals", verifyToken,(req, res) => {
+    animalSchema.find()
         .then((data) => res.json(data))
         .catch((error) => res.json({ message: error }));
 });
